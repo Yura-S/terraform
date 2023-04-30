013081a53d2522db0b081ab3e432b43d8fc6720a
Yura-S <36129839+Yura-S@users.noreply.github.com>
2023-04-30 20:47:43 +0400

    Update backend_s3.tf

--- backend_s3.tf
+++ backend_s3.tf
@@ -5,7 +5,7 @@ terraform {
     region = "us-east-1"
     kms_key_id     = "alias/terraform-bucket-key"
     dynamodb_table = "terraform-up-and-running-locks"
-    access_key = "AKIAX2264LPJALDXIW2C"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
   }
  }
